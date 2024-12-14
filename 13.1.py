76b2c46173aa97e700ee7518fa44b306818ccaab
mtarchbold <mtarchbold@gmail.com>
2024-12-14 15:42:30 -0600

    13

--- 13.1.py
+++ 13.1.py
@@ -10,27 +10,29 @@ for claw_machine in lines.split("\n\n"):
     prize_cords = re.findall(r"Prize: X=(\d+), Y=(\d+)",claw_machine)
     claw_machine = {"ax": int(buttons[0][1]), "ay": int(buttons[0][2]), "bx": int(buttons[1][1]), "by": int(buttons[1][2]),"xprize": int(prize_cords[0][0]), "yprize": int(prize_cords[0][1])}
     claw_machines.append(claw_machine)
-
+ 
 a_cost = 3
 b_cost = 1
 
 def get_solutions(ax,ay,bx,by,xprize,yprize):
-    for a in range(int(xprize/ax)):
-        xval_list = []
-        solution_list = []
+    xprize = xprize + 10000000000000
+    yprize = yprize + 10000000000000
+    xval_list = []
+    solution_list = []
+    for a in range(int(xprize/ax)+1):
         xval= ax * a
         if (xprize - xval) % bx == 0:
-            b = (xprize-xval) / bx
-            xval_list.append({"a":a, "b" : b})
+            b = (xprize-xval)/bx
+            xval_list.append({"a":a, "b":b})
         for val in xval_list:
             if ((val["a"] * ay) + (val["b"] * by)) == yprize:
                 solution_list.append(val)
-        return get_optimal_solution(solution_list)
+    return get_optimal_solution(solution_list)
 
 def get_optimal_solution(solution_list):
-    optimal_solution = 0
     if len(solution_list) == 0:
         return 0
+    optimal_solution = 0
     for solution in solution_list:
         if optimal_solution == 0 or (solution["a"] * a_cost + solution["b"]) < optimal_solution:
             optimal_solution = solution["a"] * a_cost + solution["b"]
@@ -39,4 +41,4 @@ def get_optimal_solution(solution_list):
 total_tokens = 0
 for machine in claw_machines:
     total_tokens += get_solutions(machine["ax"],machine["ay"],machine["bx"],machine["by"],machine["xprize"],machine["yprize"])
-print(total_tokens)
+print(int(total_tokens))
