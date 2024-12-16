310e20d3aec13cfabb830bac35cb227c8b9406cc
mtarchbold <mtarchbold@gmail.com>
2024-12-15 19:09:03 -0600

    6&7

---
+++ 7.2.py
@@ -0,0 +1,23 @@
+with open('7.txt') as file:
+    lines =  file.readlines()
+
+def solvable(test_value, numbers):
+    def helper(index, current_value):
+        if index == len(numbers) - 1:
+            return current_value == test_value
+        for operator in ('+', '*', '||'):
+            if operator == '||':
+                next_val = int(str(current_value)+str(numbers[index + 1]))
+            else:
+                next_val = eval(f"{current_value}{operator}{numbers[index + 1]}")
+            if helper(index + 1, next_val):
+                return True
+        return False
+    return helper(0, numbers[0])
+
+total_calibration = 0
+for line in lines:
+    test_value, *numbers = map(int, line.replace(':','').strip().split())
+    if solvable(test_value, numbers):
+        total_calibration += test_value
+print(total_calibration)
